757fba460fa1eead804dc252bd6841d02ac90730
Brett Whitson <bremwhit@ut.utm.edu>
2020-09-29 00:03:15 -0500

    moved build to front end code and changed server code to match

--- mernapp/backend/server.js
+++ mernapp/backend/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
