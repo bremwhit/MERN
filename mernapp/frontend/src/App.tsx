4a3719c745349bfbb0536b5f882160c45d048f2c
Garrett Hay <garlhay@ut.utm.edu>
2020-10-04 01:32:23 -0500

    Adding Apollo Client to React imports
    -Addition of @apollo/client module
    -Addition of graphql module
    -Modified Itembox JSX creation

--- mernapp/frontend/src/App.tsx
+++ mernapp/frontend/src/App.tsx
@@ -5,56 +5,24 @@ import ItemHeader from './components/ItemHeader';
 import ItemBox from './components/ItemBox';
 import SideBarLeft from './components/SideBarLeft';
 import QueryBar from './components/QueryBar';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 
 function App() {
+  let items = [];
+  for (let i=0;i<2;i++) {
+    items.push(<ItemBox />);
+  }
+
   return (
     <div className="App">
       <Header />
       <SideBarLeft />
       <QueryBar />
       <div className='productInfo'>
-        <ItemHeader />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-        <ItemBox />
-
+       <ItemHeader />
+       {items}
       </div>
     </div>
   );
