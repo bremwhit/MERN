770588d1d47aab71d526911f6a0e5ac51d935ff5
Garrett Hay <garlhay@ut.utm.edu>
2020-10-12 23:46:34 -0500

    Changing App to functional components & organizing queries
    -changed class 'App' to functional component
    -organized testQueries with 'queries' object & 'mutations' object

--- mernapp/frontend/src/App.tsx
+++ mernapp/frontend/src/App.tsx
@@ -6,7 +6,8 @@ import ItemBox from './components/ItemBox';
 import SideBarLeft from './components/SideBarLeft';
 import QueryBar from './components/QueryBar';
 import gqlQueries from './graphQL/testQueries';
-import {ApolloClient, InMemoryCache, gql, ApolloProvider, useQuery } from '@apollo/client';
+import { ApolloClient, InMemoryCache, gql, ApolloProvider } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 //Apollo Client setup imports: ApolloClient, InMemoryCache, gql
 //General Apollo use imports: gql, ApolloProvider, useQuery
 
@@ -25,57 +26,46 @@ GQLclient.query({
       }
     }
   `
-}).then(result => console.log("1.  " + result));
+}).then(result => console.log(result));
 
-
-
-export class App extends React.Component {
+function TestingQ() {
+  var queries = new gqlQueries();
+  var {loading, error, data } = useQuery(queries.queries.EXCHANGE_RATES);
+  if(loading) { console.log('Loading...'); }
+  if(error) { console.log(`Error ${error.message}`); }
+  else { console.log(data);}
   
-  render() {
-    //creating all item boxes to be pushed into the app
-    let itemBoxes = [];
-    for (let i=0;i<2;i++) {
-      itemBoxes.push(<ItemBox />);
-    }
-    
-    var queries = new gqlQueries();
-    var {loading, error, data } = useQuery(queries.GET_RATES);
-    if(loading) { console.log('Loading...'); }
-    if(error) { console.log(`Error ${error.message}`); }
-    else { console.log(data);}
+}
 
-    return (
-    <ApolloProvider client={GQLclient}>
-      <div className="App">
-        <Header />
-        <SideBarLeft />
-        <QueryBar />
-        <div className='productInfo'>
-          <ItemHeader />
-          {itemBoxes}
-        </div>
+function TestingM() {
+  var queries = new gqlQueries();
+  var [addTodo, { data }] = useMutation(queries.mutations.ADD_TODO);
+  console.log([addTodo, {data}]);
+}
+
+function App() {
+  //creating all item boxes to be pushed into the app
+  let itemBoxes = [];
+  for (let i=0;i<2;i++) {
+    itemBoxes.push(<ItemBox />);
+  }
+
+  //TestingQ();
+  //TestingM();
+  
+  return (
+  <ApolloProvider client={GQLclient}>
+    <div className="App">
+      <Header />
+      <SideBarLeft />
+      <QueryBar />
+      <div className='productInfo'>
+        <ItemHeader />
+        {itemBoxes}
       </div>
-    </ApolloProvider>
-    );
+    </div>
+  </ApolloProvider>
+  );
 }
-}
-
-// function App(props:object) {
-
-
-//   return (
-//     <ApolloProvider client={client}>
-//       <div className="App">
-//         <Header />
-//         <SideBarLeft />
-//         <QueryBar />
-//         <div className='productInfo'>
-//         <ItemHeader />
-//         {itemBoxes}
-//         </div>
-//       </div>
-//     </ApolloProvider>
-//   );
-// }
 
 export default App;
--- mernapp/frontend/src/graphQL/testQueries.tsx
+++ mernapp/frontend/src/graphQL/testQueries.tsx
@@ -1,13 +1,34 @@
 import { gql } from "@apollo/client";
 
 export class gqlQueries {
-  GET_RATES = gql`
+  queries = { GET_RATES : gql`
     query GetRates {
       rates(currency: "USD") {
         currency
       }
     }
-  `;
+  `,
+
+  EXCHANGE_RATES : gql`
+    query GetExchangeRates {
+      rates(currency: "USD") {
+        currency
+        rate
+      }
+    }
+  `
+  };
+
+  mutations = {
+    ADD_TODO : gql`
+      mutation AddTodo($type: String!) {
+        addTodo(type: $type) {
+          id
+          type
+        }
+      }
+    `
+  };
 
 }
 
