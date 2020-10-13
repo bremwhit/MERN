780b4ceb138e84c91b0068514dfb43abbe59ceeb
Garrett Hay <garlhay@ut.utm.edu>
2020-10-12 23:50:57 -0500

    updating gitignore & removing console debugging
    -updating gitignore to include build/ directory
    -removed debugging console log in Header file

--- mernapp/frontend/src/components/Header.tsx
+++ mernapp/frontend/src/components/Header.tsx
@@ -3,11 +3,6 @@ import Logo from '../assets/logo.png';
 
 export class Header extends Component {
     render() {
-        console.log(typeof (
-            <div className='Header'>
-                <img src={Logo} alt='logo' />
-            </div>)
-            );
         return (
             <div className='Header'>
                 <img src={Logo} alt='logo' />
